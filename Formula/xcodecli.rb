1412805095e8194fd9c48e14c2271c44c137f010
oozoofrog <oozoofrog@gmail.com>
2026-03-16 23:26:51 +0900

    xcodecli 0.5.4

--- Formula/xcodecli.rb
+++ Formula/xcodecli.rb
@@ -1,8 +1,8 @@
 class Xcodecli < Formula
   desc "Go CLI wrapper around xcrun mcpbridge"
   homepage "https://github.com/oozoofrog/xcodecli"
-  url "https://github.com/oozoofrog/xcodecli/archive/refs/tags/v0.5.3.tar.gz"
-  sha256 "2d3bcb977e13ceaebafeae675fe8dd378f15550676e38addc88ab2e45fd28395"
+  url "https://github.com/oozoofrog/xcodecli/archive/refs/tags/v0.5.4.tar.gz"
+  sha256 "d8b0fb26bd0aee1350f0aacc61214511e744f31fa6019632477094ef59fde28d"
   license "MIT"
 
   depends_on "go" => :build
